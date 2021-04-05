30f43de88bc32f712ccb3bf77215c7329463ffb9
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-04-05 21:26:41 +0800

    Initial

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a string: ");
+        String inputString = scanner.nextLine();
+
+
+        System.out.println("Total of words: " + countWords(inputString));
+        System.out.println("Total of vowels: " + countVowel(inputString));
+    }
+    public static int countWords(String words){
+        String[] wordsSplit = words.split("\\s+");
+        return wordsSplit.length;
+    }
+    public static int countVowel(String words){
+        String[] vowels = {"a","e","i","o","u"};
+        int count = 0;
+        for (int i = 0; i < words.length(); i++) {
+            for (String letter : vowels){
+                if (String.valueOf(words.charAt(i)).equals(letter))
+                    count++;
+            }
+        }
+        return count;
+    }
+}
